266b3cde411e80f917501b158e6245be419d46b5
Dana Lazar <magda.d.lazar@gmail.com>
2022-03-17 19:05:14 +0200

    deploy

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,37 @@
-import { ApolloServer } from 'apollo-server';
+// import { ApolloServer } from 'apollo-server';
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql')
 
 import typeDefs from './schema';
 import resolvers from './resolvers';
 
-const server = new ApolloServer({
-    typeDefs: typeDefs,
-    resolvers: resolvers
-});
+const path = require('path');
+const app = express();
+
+// const server = new ApolloServer({
+//     typeDefs: typeDefs,
+//     resolvers: resolvers
+// });
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: typeDefs,
+    graphiql: true,
+  }),
+);
+
+app.use(express.static('public'))
+app.get('*', (req, res)=>{
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+})
+
+// server.applyMiddleware({ app });
 
 // server.listen().then(({ url }) => {
 //     console.log(`🚀  Server ready at ${url}`);
 // });
 
-server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+app.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
 });
